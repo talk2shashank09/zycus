9e32029176927526ea71629e0a784a85ceb5fa52
talk2shashank09 <talk2shashank09@gmail.com>
2017-07-23 15:43:38 +0530

    Update shashank_script.sh

--- shashank_script.sh
+++ shashank_script.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+#assuming passwordless login is there
+#excuting the script like :-
+#/bin/bash <script name> <mention the hostnames seperated with ,>
+#Ex: /bin/bash shashank_script.sh hostname1,hostname2 
+#####################################################################
+#passing the argument in the variable
+hostnames=$@
 
+#storing the value in the variable
+echo > ./host_names
+
+for host in $(echo $hostnames | sed "s/,/ /g")
+do
+    echo "$host:22" >> ./host_names
+done
+#providing the user prompt for entering the command 
+echo "Enter the command to execute"
+read -p 'Command: ' command
+
+# ssh all servers
+
+ssh -h host_names -t 10 -i "$command"
